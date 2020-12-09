e37b5c334cb64ecd572382dbfe301b3e7b7b2612
TdWa <59452572+TdWa@users.noreply.github.com>
2020-12-09 15:10:57 +0100

    redirect to homepage after manual login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,24 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { selectToken } from "../store/user/selectors";
+import { Link, useHistory } from "react-router-dom";
 import LoginForm from "../components/LoginForm";
 
 export default function Login() {
+  const token = useSelector(selectToken);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (token !== null) {
+      history.push("/");
+    }
+  }, [token, history]);
+
   return (
     <div>
       <LoginForm />
 
-      <Link style={{ color: "black", textDecoration: "none" }} to="/signUp">
+      <Link style={{ color: "blue", textDecoration: "none" }} to="/signUp">
         Please use this link to create an account!
       </Link>
     </div>
