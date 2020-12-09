6b7d425dc53df80653b8eaba0a77e1db3fa42445
TdWa <59452572+TdWa@users.noreply.github.com>
2020-12-09 12:35:52 +0100

    add appState & dispatch to it from user actions

--- src/store/appState/actions.js
+++ src/store/appState/actions.js
@@ -1,3 +1,4 @@
+import { DEFAULT_MESSAGE_TIMEOUT } from "../../config/constants";
 export const APP_LOADING = "APP_LOADING";
 export const APP_DONE_LOADING = "APP_DONE_LOADING";
 export const SET_MESSAGE = "SET_MESSAGE";
@@ -6,29 +7,18 @@ export const CLEAR_MESSAGE = "CLEAR_MESSAGE";
 export const appLoading = () => ({ type: APP_LOADING });
 export const appDoneLoading = () => ({ type: APP_DONE_LOADING });
 export const clearMessage = () => ({ type: CLEAR_MESSAGE });
+export const setMessage = (type, text) => ({
+  // type should be "success" | "error"
+  type: SET_MESSAGE,
+  payload: { type, text },
+});
 
-export const setMessage = (variant, dismissable, text) => {
-  return {
-    type: SET_MESSAGE,
-    payload: {
-      variant,
-      dismissable,
-      text,
-    },
-  };
-};
-
-export const showMessageWithTimeout = (
-  variant,
-  dismissable,
-  text,
-  timeOutMilliSeconds
-) => {
+export const showMessageWithTimeout = (type, text, timeOutMilliSeconds) => {
   return (dispatch) => {
-    dispatch(setMessage(variant, dismissable, text));
+    dispatch(setMessage(type, text));
 
-    // const timeout = timeOutMilliSeconds || DEFAULT_MESSAGE_TIMEOUT;
+    const timeout = timeOutMilliSeconds || DEFAULT_MESSAGE_TIMEOUT;
 
-    setTimeout(() => dispatch(clearMessage()));
+    setTimeout(() => dispatch(clearMessage()), timeout);
   };
 };
--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -2,6 +2,12 @@ import Axios from "axios";
 import { apiUrl } from "../../config/constants";
 import { getCategories } from "../categories/actions";
 import { selectToken } from "./selectors";
+import {
+  appLoading,
+  appDoneLoading,
+  showMessageWithTimeout,
+  setMessage,
+} from "../appState/actions";
 
 export const loginSuccess = (response) => ({
   type: "LOGIN_SUCCESS",
@@ -13,23 +19,9 @@ const tokenStillValid = (userWithoutToken) => ({
   payload: userWithoutToken,
 });
 
-export const login = (email, password) => {
-  return async (dispatch, getState) => {
-    try {
-      const response = await Axios.post(`${apiUrl}/login`, { email, password });
-      console.log(response.data);
-      dispatch(loginSuccess(response.data));
-      dispatch(getCategories);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-};
-
-export const logOut = () => ({ type: "LOG_OUT" });
-
 export const signUp = (name, email, password) => {
   return async (dispatch, getState) => {
+    dispatch(appLoading());
     try {
       const response = await Axios.post(`${apiUrl}/signup`, {
         name,
@@ -37,23 +29,58 @@ export const signUp = (name, email, password) => {
         password,
       });
       dispatch(loginSuccess(response.data));
-    } catch (e) {
-      console.log(e);
+      dispatch(showMessageWithTimeout("success", "account created"));
+      dispatch(appDoneLoading());
+    } catch (error) {
+      if (error.response?.data?.message) {
+        console.log(error.response.data.message);
+        dispatch(setMessage("error", error.response.data.message));
+      } else {
+        console.log(error.message);
+        dispatch(setMessage("error", error.message));
+      }
+      dispatch(appDoneLoading());
     }
   };
 };
+
+export const login = (email, password) => {
+  return async (dispatch, getState) => {
+    dispatch(appLoading());
+    try {
+      const response = await Axios.post(`${apiUrl}/login`, { email, password });
+      console.log(response.data);
+      dispatch(loginSuccess(response.data));
+      dispatch(getCategories);
+      dispatch(showMessageWithTimeout("success", "welcome back!"));
+      dispatch(appDoneLoading());
+    } catch (error) {
+      if (error.response?.data?.message) {
+        console.log(error.response.data.message);
+        dispatch(setMessage("error", error.response.data.message));
+      } else {
+        console.log(error.message);
+        dispatch(setMessage("error", error.message));
+      }
+      dispatch(appDoneLoading());
+    }
+  };
+};
+
+export const logOut = () => ({ type: "LOG_OUT" });
 
 export const getUserWithStoredToken = async (dispatch, getState) => {
   const token = selectToken(getState());
   if (token === null) return;
 
+  dispatch(appLoading());
   try {
     const response = await Axios.get(`${apiUrl}/me`, {
       headers: { Authorization: `Bearer ${token}` },
     });
-    dispatch(getCategories);
-
     dispatch(tokenStillValid(response.data));
+    dispatch(getCategories);
+    dispatch(appDoneLoading());
   } catch (error) {
     if (error.response?.data?.message) {
       console.log(error.response.data.message);
@@ -61,5 +88,6 @@ export const getUserWithStoredToken = async (dispatch, getState) => {
       console.log(error.message);
     }
     dispatch(logOut());
+    dispatch(appDoneLoading());
   }
 };
